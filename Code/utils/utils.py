ff23c9abe25ee894ad090cca2c132489e0d10d5e
SamyakJha114 <125947062+SamyakJha114@users.noreply.github.com>
2024-08-28 12:48:34 +0530

    Add files via upload

---
+++ Code/utils/utils.py
@@ -0,0 +1,55 @@
+import torch
+import os
+
+def evaluate_perplexity(model, dataloader, criterion, device,domain_idx):
+    model.eval()
+    total_loss = 0.0
+    total_tokens = 0
+    with torch.no_grad():
+        for batch in dataloader:
+            inputs = batch['input_ids'].to(device)
+            labels = inputs.clone()
+            outputs = model(inputs[:, :-1], domain_idx)
+            loss = criterion(outputs.reshape(-1, outputs.size(-1)), labels[:, 1:].reshape(-1))
+            total_loss += loss.item() * labels.numel()
+            total_tokens += labels.numel()
+    print(f"average_test_loss :- {total_loss / total_tokens}")
+    perplexity = torch.exp(torch.tensor(total_loss / total_tokens))
+    return perplexity.item()
+
+def save_model(model, optimizer, path):
+    torch.save(model.state_dict(), path + '/model_state.pth')
+    torch.save(optimizer.state_dict(),path + '/optimizer_state.pth')
+
+def read_text_files(directory, file_pattern):
+    files = sorted([os.path.join(directory, f) for f in os.listdir(directory) if f.startswith(file_pattern)])
+    data = []
+    for i,file_path in enumerate(files):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                data.append(line.strip())
+    return data
+
+def train(model, dataloaders, optimizer, criterion, num_epochs, device, vocab_size, print_every=1000):
+    model.train()
+    for domain_id, dataloader in enumerate(dataloaders):
+        print(f"Training domain :- {domain_id}")
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for i, batch in enumerate(dataloader, 1):
+                inputs = batch['input_ids'].to(device)  
+                labels = inputs.clone()
+                optimizer.zero_grad()
+                outputs = model(inputs[:, :-1], domain_id)
+                loss = criterion(outputs.reshape(-1, outputs.size(-1)), labels[:, 1:].reshape(-1))
+                loss.backward()
+                optimizer.step()
+                
+                running_loss += loss.item()
+                
+                if i % print_every == 0:
+                    print(f'Epoch [{epoch+1}/{num_epochs}], Domain [{domain_id+1}/{len(dataloaders)}], Iteration [{i}], Loss: {loss.item():.4f}')
+                    
+            avg_loss = running_loss / len(dataloader)
+            print(f'Epoch [{epoch+1}/{num_epochs}], Domain [{domain_id+1}/{len(dataloaders)}], Average Loss: {avg_loss:.4f}')
+        
