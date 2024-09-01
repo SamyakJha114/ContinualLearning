1aef7f6ed2c099ff1ccb1bee586fb8e30722587a
SamyakJha114 <samyakjha71@gmail.com>
2024-09-01 19:52:42 +0200

    added third dataset

--- Code/DataCode/preprocessing_data.py
+++ Code/DataCode/preprocessing_data.py
@@ -2,6 +2,7 @@ import torch
 from datasets import load_dataset, Dataset
 import os
 import random
+import glob
 from torch.utils.data import DataLoader,random_split
 from transformers import GPT2Tokenizer
 from utils.utils import read_text_files
@@ -9,8 +10,10 @@ from utils.utils import read_text_files
 def load_and_preprocess_datasets(batch_size):
     seed = 42
     torch.manual_seed(seed)
-    dataset1 = load_dataset("krvhrv/Healix-2.8B-Token-Medical-Shot", split="train[0:10%]",cache_dir="./cache",num_proc = 32)
-    dataset2 = load_dataset("Skylion007/openwebtext", split="train[0:5%]",cache_dir="./cache",num_proc = 32,trust_remote_code=True)
+    medical_files = glob.glob('datasets/Token-Medical-Shot/train-*.parquet')
+    selected_files = random.sample(medical_files, int(0.2 * len(medical_files)))
+    dataset1 = load_dataset('parquet', data_files=selected_files, num_proc=32)
+    dataset2 = load_dataset("Skylion007/openwebtext", split="train[0:15%]",cache_dir="./cache",num_proc = 32,trust_remote_code=True)
     tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
@@ -41,7 +44,7 @@ def load_and_preprocess_datasets(batch_size):
     directory = "datasets/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled"
     file_pattern = "news.en-"
     data = read_text_files(directory, file_pattern)
-    data = random.sample(data, int(len(data) * 0.01))
+    data = random.sample(data, int(len(data) * 0.05))
     dataset3 = Dataset.from_dict({"text": data})
     tokenized_datasets3 = dataset3.map(tokenize_function, batched=True,num_proc = 64)
     tokenized_datasets3.set_format(type="torch", columns=["input_ids", "attention_mask"])
@@ -50,7 +53,7 @@ def load_and_preprocess_datasets(batch_size):
     directory1 = "datasets/1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled"
     file_pattern = "news.en-"
     data = read_text_files(directory, file_pattern)
-    data = random.sample(data, int(len(data) * 0.01))
+    data = random.sample(data, int(len(data) * 0.05))
     dataset3 = Dataset.from_dict({"text": data})
     tokenized_datasets3 = dataset3.map(tokenize_function, batched=True,num_proc = 64)
     tokenized_datasets3.set_format(type="torch", columns=["input_ids", "attention_mask"])
